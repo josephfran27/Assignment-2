0a0b81a70dfa1eae1be7fe4d7d77bafa501d710a
Joseph France <jafcxm@umsystem.edu>
2025-03-03 18:45:57 -0600

    Created a main to start testing.

---
+++ src/middleearth/Main.java
@@ -0,0 +1,12 @@
+package middleearth;
+
+import middleearthcharacters.Elf;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Elf myElf = new Elf("Legolas", 20.0, 50.0);		
+		System.out.println(myElf.getRace());
+	}
+
+}
